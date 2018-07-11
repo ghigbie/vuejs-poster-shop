fb86b02f50aaef55d20c5a6698edb33d03248bd7
ghigbie <gehigbie@gmail.com>
2018-07-10 20:33:10 -0600

    Add new method

--- public/script.js
+++ public/script.js
@@ -1,1 +1,13 @@
-console.log('It works.');
+console.log('It works well.');
+
+new Vue({
+    el: '#app',
+    data: {
+        total: 0
+    },
+    methods: {
+        addItem: funciton(){
+            this.total += 9.99;
+        }
+    }
+})
