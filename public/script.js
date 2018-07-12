4e85093cd34206e649d1dad07b1edb5c19103d34
ghigbie <gehigbie@gmail.com>
2018-07-11 20:54:21 -0600

    Add ids to objects

--- public/script.js
+++ public/script.js
@@ -5,21 +5,30 @@ new Vue({
     data: {
         total: 0.00, 
         items: [
-            {title: 'Item 1'},
-            {title: 'Item 2'},
-            {title: 'Item 3'},
-            {title: 'Item 4'},
-            {title: 'Item 5'},
+            { id: 1, title: 'Item 1'},
+            { id: 2, title: 'Item 2'},
+            { id: 3, title: 'Item 3'},
+            { id: 4, title: 'Item 4'},
+            { id: 5, title: 'Item 5'},
         ],
         imageSrc: 'http://placekitten.com/300/300',
         cart: []
     },
     methods: {
         addItem: function(index){
-            console.log(index);
             this.total += 9.99;
-            this.cart.push(this.items[index]);
-            console.log(this.cart);
+            const item = this.items[index];
+            for(let i = 0; i < cart.length; i++){
+                if(this.cart[i] === item){
+                    cart[i].qty += 1;
+                }else{
+                    this.cart.push({
+                        id: item.id,
+                        title: item.title,
+                        qty: 1
+                    });
+                }
+            }
         },
         removeItem: function(index){
             this.cart.splice(index, 1);
