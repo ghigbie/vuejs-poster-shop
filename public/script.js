a5eb29907c2e08dafd7734bc6c02a583c1504658
ghigbie <gehigbie@gmail.com>
2018-07-11 20:05:33 -0600

    Add functionality to addItem function

--- public/script.js
+++ public/script.js
@@ -3,11 +3,26 @@ console.log('It works well.');
 new Vue({
     el: '#app',
     data: {
-        total: 0
+        total: 0.00, 
+        items: [
+            {title: 'Item 1'},
+            {title: 'Item 2'},
+            {title: 'Item 3'},
+            {title: 'Item 4'},
+            {title: 'Item 5'},
+        ],
+        imageSrc: 'http://placekitten.com/300/300',
+        cart: []
     },
     methods: {
-        addItem: funciton(){
+        addItem: function(index){
+            console.log(index);
             this.total += 9.99;
+            this.cart.push(this.items[index]);
+            console.log(this.cart);
+        },
+        removeItem: function(index){
+
         }
     }
 })
